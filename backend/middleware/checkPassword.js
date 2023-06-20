2f54e31c8909e68c075a74407ee68be4422829f9
Houda Ben Abdallah <ahhouda@yahoo.fr>
2023-06-20 19:51:14 +0200

    validator password

--- backend/middleware/checkPassword.js
+++ backend/middleware/checkPassword.js
@@ -1,7 +1,16 @@
 const passwordValidator = require('password-validator');
-
-module.exports = (red , res, next) =>{
-      console.log('password ok')
-      next()
-      
+const passwordschema = new passwordValidator();
+      passwordschema
+      .is().min(8)                                    // Minimum length 8
+      .is().max(20)                                   // Maximum length 20
+      .has().uppercase()                              // Must have uppercase letters
+      .has().lowercase()                              // Must have lowercase letters
+      .has().digits(1)                                // Must have at least 1 digits
+      .has().not().spaces()                           // Should not have spaces
+module.exports = (req , res, next) =>{
+      if(passwordschema.validate(req.body.password)){
+            next()
+      } else {
+            return res.status(400).json({error : `le mot de passe n'est pas assez fort: ${passwordschema.validate('req.body.password' , { list: true })}`})
+       }
 }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,10 @@
-
 const express = require('express');
-const router = express.Router();
-const checkMail = require('../middleware/checkMail');
-//const checkPassword = require ('../middleware/checkPassword');
-
 const userCtrl = require('../controllers/user');
+const checkMail = require('../middleware/checkMail');
+const checkPassword = require ('../middleware/checkPassword');
+const router = express.Router();
 
-router.post('/signup', checkMail, userCtrl.signup);// middlewvre verifie mail et mot de passe   checkMail,checkPassword,
+router.post('/signup', checkMail, checkPassword, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
