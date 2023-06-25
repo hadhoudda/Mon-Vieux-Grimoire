2da79302cd9b87bb961da028aa57ce8b07c7edaa
Houda Ben Abdallah <ahhouda@yahoo.fr>
2023-06-25 20:51:29 +0200

    update book

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post('/', auth , multer , bookCtrl.createBook);
 // Route pour ajouter une évaluation à un livre
 router.post('/:id/rating', auth , bookCtrl.creatRating)
-// Route pour mettre à jour les informations d'un livre existant
+// Route pour modifier les informations d'un livre existant
 router.put('/:id', auth , multer , bookCtrl.modifyBook);
 // Route pour supprimer un livre existant
 router.delete('/:id', auth , bookCtrl.deleteBook);
