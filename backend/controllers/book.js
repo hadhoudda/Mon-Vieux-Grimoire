a8f10af5119fe091d86fea46ee57b3154498e7a5
Houda Ben Abdallah <ahhouda@yahoo.fr>
2023-06-29 00:43:23 +0200

    modify type picture at webp

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -10,7 +10,6 @@ exports.createBook = (req, res, next) => {
     userId: req.auth.userId,
     imageUrl: `${req.protocol}://${req.get("host")}/images/${ req.file.filename }`, 
     });
-    console.log(book)
 
     book.save()
       .then(() => res.status(201).json({ message: 'Livre enregistré !'}))
--- backend/middleware/sharp.js
+++ backend/middleware/sharp.js
@@ -1,5 +1,21 @@
 const sharp = require ('sharp')
+const path = require("path")
+const fs = require('fs')
 
 module.exports= (req, res, next) => {
+      if(req.file){
+            console.log(req.file)
+            //change type d'image en webp
+            const newFileName = req.file.filename.split(".")[0]
+            req.file.filename = newFileName + ".webp"
+            console.log(req.file.filename)
+            //compresse image 
+            //sharp(req.file.path).resize(300).toFile()       
+            console.log(req.file)
+            //supprime l'image non compresse
+            fs.unlinkSync(req.file.path)
+
+      }
       
+      next()
 }
