edf3d8770d66451ad5b37cb6b0f1dbe6cd2a53d5
Houda Ben Abdallah <ahhouda@yahoo.fr>
2023-07-05 19:48:08 +0200

    update file .gitignore

--- backend/middleware/sharp.js
+++ backend/middleware/sharp.js
@@ -4,15 +4,12 @@ const fs = require('fs')
 
 module.exports= async (req, res, next) => {
       if(req.file){
-            console.log(req.file)
             //change type d'image en webp
             const newFileName = req.file.filename.split(".")[0]
             req.file.filename = newFileName + ".webp"
             const newName =newFileName + ".webp"
             await sharp(req.file.path).resize(400).toFile(path.resolve(req.file.destination, newName))
-            console.log(req.file)
             fs.unlinkSync(req.file.path)
-      }
-      
+      }      
       next()
 }
